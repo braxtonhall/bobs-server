47705e735bc981343f45dac06b27eee54c97b10c
braxtonhall <braxtonjhall@gmail.com>
2024-03-24 01:04:55 +0100

    Make the Config lazy

--- src/Config.ts
+++ src/Config.ts
@@ -1,24 +1,36 @@
 import "dotenv/config";
 import { z } from "zod";
 
-const warned =
-	<D>(key: string, defaultValue: D) =>
-	() => {
+type Config = {
+	[K in keyof typeof environmentSchema]: z.infer<ReturnType<(typeof environmentSchema)[K]>>;
+};
+
+const requiredString = () => z.string();
+
+const numberWithDefault = (defaultValue: number) => (key: string) =>
+	z.coerce.number().default(() => {
 		console.warn(`Environment variable "${key}" was not set. Using default value: "${defaultValue}"`);
 		return defaultValue;
-	};
+	});
 
-const Config = z
-	.object({
-		SSL_CERT_PATH: z.string(),
-		SSL_KEY_PATH: z.string(),
-		HTTP_PORT: z.coerce.number().default(warned("HTTP_PORT", 80)),
-		HTTPS_PORT: z.coerce.number().default(warned("HTTPS_PORT", 443)),
-		DEFAULT_PAGE_SIZE: z.coerce.number().default(warned("DEFAULT_PAGE_SIZE", 20)),
-		MAXIMUM_PAGE_SIZE: z.coerce.number().default(warned("MAXIMUM_PAGE_SIZE", 100)),
-		DELETION_TIME_MS: z.coerce.number().default(warned("DELETION_TIME_MS", 1000 * 60 * 10)),
-		KARMA_KILL_THRESHOLD: z.coerce.number().default(warned("KARMA_KILL_THRESHOLD", 5)),
-	})
-	.parse(process.env);
+const environmentSchema = {
+	SSL_CERT_PATH: requiredString,
+	SSL_KEY_PATH: requiredString,
+	HTTP_PORT: numberWithDefault(80),
+	HTTPS_PORT: numberWithDefault(443),
+	DEFAULT_PAGE_SIZE: numberWithDefault(20),
+	MAXIMUM_PAGE_SIZE: numberWithDefault(100),
+	DELETION_TIME_MS: numberWithDefault(1000 * 60 * 10),
+	KARMA_KILL_THRESHOLD: numberWithDefault(5),
+};
+
+const validatedConfig: Record<string, unknown> = {};
+
+const Config = new Proxy({} as Config, {
+	get(_: Config, key: keyof Config): Config[typeof key] {
+		validatedConfig[key] ??= environmentSchema[key](key).parse(process.env[key]);
+		return validatedConfig[key] as Config[typeof key];
+	},
+});
 
 export default Config;
