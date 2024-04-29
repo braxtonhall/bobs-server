9096c26aa2469b249763ad8ba17ca2fd02cc2e18
braxtonhall <braxtonjhall@gmail.com>
2024-04-28 17:07:31 -0700

    Submit playlist

--- src/secret-dj/operations/submitPlaylist.ts
+++ src/secret-dj/operations/submitPlaylist.ts
@@ -7,7 +7,7 @@ type Environment = {
 	playlist: string;
 };
 
-const submitPlaylist = async ({ season, dj, playlist }: Environment) => {
+export const submitPlaylist = async ({ season, dj, playlist }: Environment) => {
 	const entry = await db.entry.findFirst({
 		where: {
 			seasonId: season,
@@ -23,14 +23,14 @@ const submitPlaylist = async ({ season, dj, playlist }: Environment) => {
 		},
 	});
 	if (entry === null) {
-		// TODO
-	} else if (entry.season.state !== SeasonState.IN_PROGRESS)
-		if (entry) {
-			await db.entry.update({
-				where: { id: entry.id },
-				data: { submissionUrl: playlist },
-			});
-		} else {
-			// TODO
-		}
+		throw new Error(`Entry for season id ${season} does not exist`);
+	}
+	const result = await db.entry.update({
+		where: { id: entry.id, season: { state: SeasonState.IN_PROGRESS } },
+		data: { submissionUrl: playlist },
+		select: {},
+	});
+	if (result === null) {
+		throw new Error(`Season ${season} is not in progress and cannot be updated`);
+	}
 };
