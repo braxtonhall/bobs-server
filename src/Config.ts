0ef14cdecc9200a27be3a5a8e5d34a6f597dd63d
braxtonhall <braxtonjhall@gmail.com>
2024-05-19 01:53:08 -0700

    Make the config eager

--- src/Config.ts
+++ src/Config.ts
@@ -1,24 +1,14 @@
 import "dotenv/config";
 import { z } from "zod";
 
-type Config = {
-	[K in keyof typeof environmentSchema]: z.infer<ReturnType<(typeof environmentSchema)[K]>>;
-};
+const numberWithDefault = (defaultValue: number) => z.coerce.number().default(defaultValue);
 
-const requiredString = () => z.string();
-
-const numberWithDefault = (defaultValue: number) => (key: string) =>
-	z.coerce.number().default(() => {
-		console.warn(`Environment variable "${key}" was not set. Using default value: "${defaultValue}"`);
-		return defaultValue;
-	});
-
-const environmentSchema = {
-	SSL_CERT_PATH: requiredString,
-	SSL_KEY_PATH: requiredString,
-	SENDGRID_API_KEY: requiredString,
-	JWT_SECRET: requiredString,
-	HOST: requiredString,
+const environmentSchema = z.object({
+	SSL_CERT_PATH: z.string(),
+	SSL_KEY_PATH: z.string(),
+	SENDGRID_API_KEY: z.string(),
+	JWT_SECRET: z.string(),
+	HOST: z.string(),
 	TEMP_TOKEN_EXPIRATION_MIN: numberWithDefault(10),
 	API_TOKEN_EXPIRATION_HOURS: numberWithDefault(12),
 	HTTP_PORT: numberWithDefault(80),
@@ -27,15 +17,8 @@ const environmentSchema = {
 	MAXIMUM_PAGE_SIZE: numberWithDefault(100),
 	DELETION_TIME_MS: numberWithDefault(1000 * 60 * 10),
 	KARMA_KILL_THRESHOLD: numberWithDefault(5),
-};
-
-const validatedConfig: Record<string, unknown> = {};
-
-const Config = new Proxy({} as Config, {
-	get(_: Config, key: keyof Config): Config[typeof key] {
-		validatedConfig[key] ??= environmentSchema[key](key).parse(process.env[key]);
-		return validatedConfig[key] as Config[typeof key];
-	},
 });
+
+const Config = environmentSchema.parse(process.env);
 
 export default Config;
