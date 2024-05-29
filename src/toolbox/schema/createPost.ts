0400ef6b7e6e6d1816871f9abdec6988e2924570
braxtonhall <braxtonjhall@gmail.com>
2024-05-29 01:31:17 -0700

    Some validation on the email input

--- src/toolbox/schema/createPost.ts
+++ src/toolbox/schema/createPost.ts
@@ -3,14 +3,17 @@ import { emailSchema } from "./email";
 import { Option, None, Some } from "../../types/option";
 
 export const createPostSchema = z.object({
-	from: z.string(),
-	email: emailSchema.optional().transform((email): Option<string> => {
-		if (email) {
-			return Some(email);
-		} else {
-			return None();
-		}
-	}),
+	from: z.string().trim(),
+	email: emailSchema
+		.or(z.literal(""))
+		.optional()
+		.transform((email): Option<string> => {
+			if (email) {
+				return Some(email);
+			} else {
+				return None();
+			}
+		}),
 	content: z.string(),
 	parent: z.string().optional(),
 });
--- src/toolbox/schema/email.ts
+++ src/toolbox/schema/email.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const emailSchema = z
 	.string()
-	.transform((email) => email.trim())
+	.trim()
 	.refine((email) => /^\S+@\S+\.\S+$/.test(email), {
 		message: "Email should resemble an email address",
 	});
