f1805fd3ee8d17beccd2567dd289abbc43b66138
braxtonhall <braxtonjhall@gmail.com>
2024-06-06 11:03:40 -0700

    Add a lot of console output

--- src/email.ts
+++ src/email.ts
@@ -10,6 +10,7 @@ export type Message = Omit<PrismaMessage, "id" | "expiration"> & Partial<Pick<Pr
 
 export const enqueue = async (client: Pick<typeof db, "message">, ...messages: Message[]): Promise<void> => {
 	await client.message.createMany({ data: messages });
+	console.log(new Date().toLocaleString(), `Enqueuing ${messages.length} messages`);
 	void sendQueuedMessages();
 };
 
@@ -17,29 +18,39 @@ const locks = new Set<number>();
 
 export const sendQueuedMessages = async (errorDelay = 1000) => {
 	try {
+		console.log(new Date().toLocaleString(), `Dequeue message begin`);
 		const message = await db.message.findFirst({ orderBy: { id: "asc" } });
 		if (message) {
+			console.log(new Date().toLocaleString(), `Pulled ${message.address} off the queue`);
 			if (!locks.has(message.id)) {
 				try {
+					console.log(new Date().toLocaleString(), `Locking message ${message.address}`);
 					locks.add(message.id);
 					if (message.expiration === null || message.expiration >= new Date()) {
 						await sendMessage(message);
+						console.log(new Date().toLocaleString(), "Message for", message.address, "sent!");
 					} else {
-						console.log("Message for", message.address, "has expired");
+						console.log(new Date().toLocaleString(), "Message for", message.address, "has expired");
 					}
 					await db.message.deleteMany({ where: { id: message.id } });
 					setImmediate(sendQueuedMessages);
 				} catch (error) {
-					console.error("Unexpected error", error);
+					console.error(new Date().toLocaleString(), "Unexpected error", error);
 					await setTimeout(errorDelay);
 					setImmediate(sendQueuedMessages, errorDelay * 2);
 				} finally {
+					console.log(new Date().toLocaleString(), `Unlocking message ${message.address}`);
 					locks.delete(message.id);
 				}
+			} else {
+				console.log(new Date().toLocaleString(), `Message ${message.address} is already being serviced`);
 			}
+		} else {
+			console.log(new Date().toLocaleString(), `Nothing to dequeue`);
 		}
-	} catch {
+	} catch (error) {
 		// Do nothing
+		console.log(new Date().toLocaleString(), `Failed`, error);
 	}
 };
 
@@ -56,7 +67,7 @@ const sendMessage = async (message: Message): Promise<void> => {
 			})
 			.catch((error) => {
 				if (error?.code === 400) {
-					console.log("Bad request at", message.address);
+					console.log(new Date().toLocaleString(), "Bad request at", message.address);
 				} else {
 					throw error;
 				}
