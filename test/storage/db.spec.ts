98b13c60f67d67cea2e9acab7ba6090f815896dd
Braxton Hall <35436247+braxtonhall@users.noreply.github.com>
2024-06-11 22:58:23 -0700

    Emails should be case-insensitive (#52)

---
+++ test/storage/db.spec.ts
@@ -0,0 +1,71 @@
+import { dropTables } from "../util";
+import { db, transaction } from "../../src/db";
+import { Email } from "@prisma/client";
+
+describe("transactions", () => {
+	beforeEach(dropTables);
+
+	it("should not drop an operation that occurred outside a transaction", async () => {
+		await (async () => {
+			await db.email.create({ data: { address: "foo@bar.ca" } });
+			throw new Error("SOME ERROR");
+		})().catch(() => {});
+		expect(await db.email.findUnique({ where: { address: "foo@bar.ca" } })).not.toBeNull();
+	});
+
+	it("should drop an operation that occurred inside a transaction", async () => {
+		await transaction(async () => {
+			await db.email.create({ data: { address: "foo@bar.ca" } });
+			throw new Error("SOME ERROR");
+		}).catch(() => {});
+		expect(await db.email.findUnique({ where: { address: "foo@bar.ca" } })).toBeNull();
+	});
+});
+
+describe("email", () => {
+	beforeAll(dropTables);
+
+	it("should be able to create an email", async () => {
+		const email = await db.email.create({ data: { address: "foo@bar.ca" } });
+		expect(email).toEqual({
+			id: expect.any(String),
+			subscribed: true,
+			address: "foo@bar.ca",
+			confirmed: false,
+		} satisfies Email);
+	});
+
+	it("should be able to find the email using a different case", async () => {
+		const email = await db.email.findUnique({ where: { address: "Foo@bar.ca" } });
+		expect(email).toEqual({
+			id: expect.any(String),
+			subscribed: true,
+			address: "foo@bar.ca",
+			confirmed: false,
+		} satisfies Email);
+	});
+
+	it("should disallow creating a new email using a different case", async () =>
+		expect(db.email.create({ data: { address: "Foo@bar.ca" } })).rejects.toThrow());
+
+	it("should connect to an existing email if using a different case", async () => {
+		const address = "Foo@bar.ca";
+		const email = await db.email.upsert({ where: { address }, update: {}, create: { address } });
+		expect(email).toEqual({
+			id: expect.any(String),
+			subscribed: true,
+			address: "foo@bar.ca",
+			confirmed: false,
+		} satisfies Email);
+	});
+
+	it("should preserve case", async () => {
+		const email = await db.email.create({ data: { address: "Bar@Foo.ca" } });
+		expect(email).toEqual({
+			id: expect.any(String),
+			subscribed: true,
+			address: "Bar@Foo.ca",
+			confirmed: false,
+		} satisfies Email);
+	});
+});
