f27604d7040b7eee0960b48677ef1957d0504e61
braxtonhall <braxtonjhall@gmail.com>
2024-06-12 13:35:39 -0700

    A box can NOT have an owner

--- prisma/migrations/20240612203256_replies/migration.sql
+++ prisma/migrations/20240612203256_replies/migration.sql
@@ -22,5 +22,19 @@ INSERT INTO "new_Post" ("boxId", "content", "createdAt", "dead", "emailId", "fro
 DROP TABLE "Post";
 ALTER TABLE "new_Post" RENAME TO "Post";
 CREATE UNIQUE INDEX "Post_id_key" ON "Post"("id");
+CREATE TABLE "new_Box" (
+    "sort" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "origin" TEXT,
+    "ownerId" TEXT,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+    "stylesheet" TEXT,
+    CONSTRAINT "Box_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Email" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Box" ("deleted", "id", "name", "origin", "ownerId", "sort", "stylesheet") SELECT "deleted", "id", "name", "origin", "ownerId", "sort", "stylesheet" FROM "Box";
+DROP TABLE "Box";
+ALTER TABLE "new_Box" RENAME TO "Box";
+CREATE UNIQUE INDEX "Box_id_key" ON "Box"("id");
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
