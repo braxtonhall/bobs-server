a7c7561f4a783644b785134866bce94da6b638b8
Braxton Hall <35436247+braxtonhall@users.noreply.github.com>
2024-09-21 11:46:25 -0700

    Update prisma and typescript (#71)

--- src/types/result.ts
+++ src/types/result.ts
@@ -13,12 +13,12 @@ type Err<E> = { type: "err"; value: E };
 export type Result<T, E> = Ok<T> | Err<E>;
 
 export const map = <T, E, U>(result: Result<T, E>, callback: (value: T) => U): Result<U, E> =>
-	match(result)
+	match(result as any) // Type instantiation is excessively deep and possibly infinite.
 		.with(Ok(P.select()), (value): Ok<U> => Ok(callback(value as T)))
 		.otherwise((error: Err<E>) => error);
 
 export const unwrapOr = <T, E, U>(result: Result<T, E>, or: (e: E) => U): T | U =>
-	match(result)
+	match(result as any) // Type instantiation is excessively deep and possibly infinite.
 		.with(Ok(P.select()), (value): T => value as T)
 		.otherwise(or);
 
