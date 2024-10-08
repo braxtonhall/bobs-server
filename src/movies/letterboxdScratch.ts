3b394d7a4cea8d909d8afe4be5fbb89f853c27d5
braxtonhall <braxtonjhall@gmail.com>
2024-10-09 01:03:09 +0200

    Just want to write down that I had this

--- src/movies/letterboxdScratch.ts
+++ src/movies/letterboxdScratch.ts
@@ -1,55 +1,130 @@
 import { DateTime } from "luxon";
 import { XMLParser } from "fast-xml-parser";
+import { z } from "zod";
+import xray from "./util/x-ray";
+import Config from "../Config";
+import { List } from "@prisma/client";
+import { db } from "../db";
 
-const getWatchedMovies = async (userName: string, since: DateTime) => {
-	// TODO https://letterboxd.com/${userName}/rss
-	//  resolves with a list of the movies that the user has seen since `since`
-	const response = await fetch(`https://letterboxd.com/${userName}/rss`);
-	const xml = await response.text();
-	const result = new XMLParser().parse(xml);
-	/** movie example
-	 *   {
-	 *     title: 'The River, 1997',
-	 *     link: 'https://letterboxd.com/braxtonhall/film/the-river-1997/',
-	 *     guid: 'letterboxd-watch-681205961',
-	 *     pubDate: 'Mon, 30 Sep 2024 05:28:57 +1300',
-	 *     'letterboxd:watchedDate': '2024-09-29',
-	 *     'letterboxd:rewatch': 'No',
-	 *     'letterboxd:filmTitle': 'The River',
-	 *     'letterboxd:filmYear': 1997,
-	 *     'tmdb:movieId': 54291,
-	 *     description: ' <p><img src="https://a.ltrbxd.com/resized/film-poster/1/1/7/8/2/11782-the-river-0-600-0-900-crop.jpg?v=992341c945"/></p> <p>Watched on Sunday September 29, 2024.</p> ',
-	 *     'dc:creator': 'bob&#039;s son [sic]'
-	 *   },
-	 */
+const watchedMovieSchema = z
+	.object({
+		pubDate: z
+			.string()
+			.transform((date) => DateTime.fromRFC2822(date))
+			.transform((date, ctx) => {
+				if (date.isValid) {
+					return date;
+				} else {
+					ctx.addIssue({
+						code: z.ZodIssueCode.custom,
+						message: "Date format is incorrect",
+					});
+					return z.NEVER;
+				}
+			}),
+		link: z.string(),
+		"tmdb:movieId": z.number(),
+	})
+	.passthrough();
 
-	/** list example
-	 *   {
-	 *     title: 2021,
-	 *     link: 'https://letterboxd.com/braxtonhall/list/2021/',
-	 *     guid: 'letterboxd-list-31448919',
-	 *     pubDate: 'Tue, 11 Apr 2023 01:46:43 +1200',
-	 *     description: ' <p>no particular order</p> <ul> <li> <a href="https://letterboxd.com/film/nomadland/">Nomadland</a> </li> <li> <a href="https://letterboxd.com/film/alien/">Alien</a> </li> <li> <a href="https://letterboxd.com/film/dazed-and-confused/">Dazed and Confused</a> </li> <li> <a href="https://letterboxd.com/film/la-haine/">La Haine</a> </li> <li> <a href="https://letterboxd.com/film/seven-samurai/">Seven Samurai</a> </li> <li> <a href="https://letterboxd.com/film/dr-strangelove-or-how-i-learned-to-stop-worrying-and-love-the-bomb/">Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb</a> </li> </ul> ',
-	 *     'dc:creator': 'bob&#039;s son [sic]'
-	 *   }
-	 */
+const rssResponseSchema = z.object({
+	rss: z.object({
+		channel: z.object({
+			item: z.array(z.unknown()).transform((items) =>
+				items
+					.map((item) => watchedMovieSchema.safeParse(item))
+					.filter((item) => item.success)
+					.map((item) => item.data),
+			),
+		}),
+	}),
+});
 
-	return result.rss.channel.item;
+const getWatchedMovies = async (userName: string, since: DateTime): Promise<{ slug: string; tmdbId: number }[]> => {
+	try {
+		const response = await fetch(`https://letterboxd.com/${userName}/rss`);
+		const xml = await response.text();
+		const result = rssResponseSchema.parse(new XMLParser().parse(xml));
+		const moviesSince = result.rss.channel.item.filter((movie) => movie.pubDate > since);
+		return moviesSince.map((movie) => {
+			const slug = new URL(movie.link).pathname.split("/")[3] ?? "";
+			return {
+				slug,
+				tmdbId: movie["tmdb:movieId"],
+			};
+		});
+	} catch {
+		return [];
+	}
 };
 
-const getPage = async (list: string, page: number) => {
-	// TODO resolves with a list of movies, as well as some metadata like number of pages/films total
+type ScrapeResult = {
+	metadata: { count: number | null; watchlist: boolean; owner?: string; name?: string };
+	movies: { slug: string; index?: number }[];
 };
 
-const getList = async (list: string) => {
+const scrapeList = async (list: string): Promise<ScrapeResult> =>
+	xray(list, {
+		metadata: {
+			count: ".js-watchlist-count | parseInt",
+			name: "h1.title-1",
+			owner: "body@data-owner",
+			watchlist: "body.watchlist | exists",
+		},
+		movies: xray("li.poster-container", [
+			{
+				slug: "> div.film-poster@data-film-slug",
+				index: "> p.list-number | parseInt",
+			},
+		])
+			.paginate(".next@href")
+			.limit(Config.LETTERBOXD_LIST_PAGE_LIMIT),
+	});
+
+const getList = async (list: string): Promise<ScrapeResult> => {
 	// TODO get the first page. assess how many pages there are. get the remaining pages
 	//  if the number of films found matches the expected number of films, exit
 	//  else do a second search just in case :)
+	const { metadata, movies } = await scrapeList(list);
+	// TODO if movies.length === LIMIT * 28, return
+	// TODO if movies.length !== metadata.count, rerun
+	// TODO if movies.some movie => movie.index !== index + 1, rerun
+	return { metadata, movies };
 };
 
-const run = async () => {
-	return getWatchedMovies("braxtonhall", DateTime.now());
+const updateList = async (link: string) => {
+	const now = DateTime.now();
+	const list = await db.list.upsert({
+		where: { link },
+		update: {},
+		create: {
+			link,
+			updatedAt: now.minus({ day: Config.LETTERBOXD_LIST_UPDATE_INTERVAL_DAYS }).toJSDate(),
+			name: "loading...",
+		},
+	});
+
+	// if (list.watchlist && list.owner) {
+	// 	const watched = await getWatchedMovies(list.owner, DateTime.fromJSDate(list.updatedAt));
+	// 	// TODO for each of these, we can freely attach a tmdbId
+	// 	await db.listEntry.deleteMany({
+	// 		where: { listLink: link, movieSlug: { in: watched.map(({ slug }) => slug) } },
+	// 	});
+	// }
+
+	const details = await getList(link);
+
+	await db.list.update({
+		where: {
+			link,
+		},
+		data: {
+			watchlist: details.metadata.watchlist,
+		},
+	});
 };
+
+const run = async () => scrapeList("https://letterboxd.com/journal/");
 
 if (require.main === module) {
 	run()
--- src/movies/util/x-ray.ts
+++ src/movies/util/x-ray.ts
@@ -5,6 +5,8 @@ export default XRay({
 		trim: (value) => (typeof value === "string" ? value.trim() : value),
 		cleanTitle: (value) => (typeof value === "string" ? value.replace(/\(.*\)$/, "") : value),
 		normalizeWhitespace: (value) => (typeof value === "string" ? value.replace(/\s+/g, " ") : value),
+		parseInt: (value) => (typeof value === "string" ? parseInt(value.replace(/[^0-9]/g, "")) : value),
+		exists: (value) => !!value,
 	},
 })
 	.concurrency(10)
